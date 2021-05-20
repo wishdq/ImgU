192582977fcd7655b76d507130ed1b1eb849b6c6
未雨初晴 <63840807+wishdq@users.noreply.github.com>
2021-05-20 10:29:07 +0800

    Update get-bing-wallpapers.py

--- get-bing-wallpapers.py
+++ get-bing-wallpapers.py
@@ -24,7 +24,7 @@ def main():
     Destination filename will be YYYY-mm-dd_{md5dum}.jpg
     """
     dest = './assets/BingWallpapers'
-    bing_url = 'https://cn.bing.com/?FORM=BEHPTB'
+    bing_url = 'https://www.bing.com/?FORM=BEHPTB&mkt=zh-CN'
     archive_dir = os.path.join(dest, 'Archive')
 
     try:
